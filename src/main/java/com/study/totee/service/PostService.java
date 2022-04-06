8eee96d29f0cc9b02879b26a40251e28543ca151
rnjstmdals6 <rnjstmdals6@naver.com>
2022-04-06 19:08:49 +0900

    post 업데이트 및 삭제기능 api 추가

--- src/main/java/com/study/totee/service/PostService.java
+++ src/main/java/com/study/totee/service/PostService.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
-
+    private final UserRepository userRepository;
     @Transactional
     public void save(PostEntity postEntity) throws IOException {
         postRepository.save(postEntity);
@@ -28,7 +28,34 @@ public class PostService {
 
     @Transactional(readOnly = true)
     public Page findPostAll(final Pageable pageable){
-
         return postRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public PostEntity findByPostId(long postId){
+        return postRepository.findByPostId(postId);
+    }
+
+    @Transactional
+    public void updateView(Long postId){
+        PostEntity post = postRepository.findByPostId(postId);
+        post.setView(post.getView()+1);
+    }
+
+    @Transactional
+    public PostEntity update(PostDTO postDTO, Long postId, String userId){
+        Optional<UserEntity> user = userRepository.findById(userId);
+        PostEntity post = postRepository.findByPostIdAndUser(postId, user.get());
+        post.setIntro(postDTO.getIntro());
+        post.setContent(postDTO.getContent());
+        post.setTitle(postDTO.getTitle());
+        return post;
+    }
+
+    @Transactional
+    public void delete(Long postId, String userId){
+        Optional<UserEntity> user = userRepository.findById(userId);
+        PostEntity post = postRepository.findByPostIdAndUser(postId, user.get());
+        postRepository.delete(post);
     }
 }
