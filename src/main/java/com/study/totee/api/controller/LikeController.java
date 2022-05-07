cc55d26be7bfc914a7a2d3b5f4935458c513c008
rnjstmdals6 <rnjstmdals6@naver.com>
2022-05-07 16:33:37 +0900

    chore athenticationPricipal 매개변수 변경

--- src/main/java/com/study/totee/api/controller/LikeController.java
+++ src/main/java/com/study/totee/api/controller/LikeController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,16 +24,16 @@ public class LikeController {
 
     @ApiOperation(value = "좋아요!", notes = "이미 좋아요 되어있으면 좋아요는 취소됩니다.")
     @PostMapping("/api/v1/post/{postId}/like")
-    public ApiResponse like(@AuthenticationPrincipal String id , @PathVariable Long postId){
-        likeService.like(id, postId);
+    public ApiResponse like(@AuthenticationPrincipal User principal , @PathVariable Long postId){
+        likeService.like(principal.getUsername(), postId);
         return ApiResponse.success("message","SUCCESS");
     }
 
     @ApiOperation(value = "좋아요한 글 리스트", notes = "좋아요 누른 글의 리스트를 조회합니다")
     @GetMapping("/api/v1/post/like")
-    public ApiResponse myLikePost(@AuthenticationPrincipal String userId, @PageableDefault
+    public ApiResponse myLikePost(@AuthenticationPrincipal User principal, @PageableDefault
             (size = 16, sort = "postId", direction = Sort.Direction.DESC) Pageable pageable){
-        Page<PostEntity> page = likeService.findAllByLikedPost(userId, pageable);
+        Page<PostEntity> page = likeService.findAllByLikedPost(principal.getUsername(), pageable);
         Page<PostDTO> map = page.map(post -> new PostDTO(post.getUser().getUsername(), post.getView(), post.getPostId(), post.getTitle(), post.getContent()
                 , post.getCategory().getCategoryName(), post.getLike().size(), post.getComment().size(), null));
         return ApiResponse.success("data", map);
