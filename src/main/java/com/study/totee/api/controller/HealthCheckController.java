17feba3b305ff45d08cdfaf4f253a49aadf07062
rnjstmdals6 <rnjstmdals6@naver.com>
2022-05-15 00:21:13 +0900

    CICD TEST

--- src/main/java/com/study/totee/api/controller/HealthCheckController.java
+++ src/main/java/com/study/totee/api/controller/HealthCheckController.java
@@ -9,6 +9,6 @@ public class HealthCheckController {
     // AWS 로드벨런서는 기본 경로 "/"에 HTTP 요청을 보내 애플리케이션 동작 확인
     @GetMapping("/")
     public String healthCheck(){
-        return "The service is up and running...";
+        return "The service is up and running..";
     }
 }
