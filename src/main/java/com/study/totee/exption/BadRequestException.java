4b95b026d8f1a87b0a0cd77244173e2b47577593
rnjstmdals6 <rnjstmdals6@naver.com>
2022-07-05 20:48:10 +0900

    Feat : 예외처리를 위한 상태코드 작성

---
+++ src/main/java/com/study/totee/exption/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.study.totee.exption;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+    }
+
+}
