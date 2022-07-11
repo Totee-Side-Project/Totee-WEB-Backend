192416632efcff6d2889906f310c8ff207c4caae
rnjstmdals6 <rnjstmdals6@naver.com>
2022-07-11 22:58:03 +0900

    Feat : 유저 정보수정 API 추가

---
+++ src/main/java/com/study/totee/api/dto/user/UserInfoUpdateRequestDto.java
@@ -0,0 +1,24 @@
+package com.study.totee.api.dto.user;
+
+import com.study.totee.type.PositionType;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoUpdateRequestDto {
+    @ApiModelProperty(example = "닉네임")
+    private String nickname;
+    @ApiModelProperty(example = "포지션 ex) FRONT_END, BACK_END, DESIGN, GAME, ML, PRODUCT_MANAGER, iOS, ANDROID, OTHERS")
+    private PositionType position;
+    @ApiModelProperty(example = "유저 프로필 이미지")
+    private MultipartFile profileImage;
+    @ApiModelProperty(example = "변경할 닉네임")
+    private String newNickname;
+}
